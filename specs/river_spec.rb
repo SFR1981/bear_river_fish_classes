bba6aff2b328d0476be7dda734cf1d7ed9e0e640
Stephen Rooney <s.f.rooney@gmail.com>
2018-05-29 16:24:56 +0100

    add river name

--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -6,5 +6,17 @@ require_relative("../river.rb")
 class RiverTest < MiniTest::Test
 
   def setup
-    
+    @river = River.new("Amazon")
   end
+
+
+  def test_river_has_name
+
+    assert_equal("Amazon", @river.name() )
+
+
+  end
+
+
+
+end
